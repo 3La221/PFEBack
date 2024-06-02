2a315dc28805147c73384dfb4995662afe960ef3
3La221 <abacha3la2@gmail.com>
2024-06-02 21:29:49 +0100

    '30jrs'

--- gestion_medicale/settings.py
+++ gestion_medicale/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
